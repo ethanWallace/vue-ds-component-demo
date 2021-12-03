56d0f171efd8efbdc7a245ddd4dfd31332d02c48
Ethan Wallace <ethan.wallace.91@gmail.com>
2021-12-03 11:53:51 -0500

    Add gc-ds-button

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
+
+    <gc-ds-button label="Look at me"></gc-ds-button>
+
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import { applyPolyfills, defineCustomElements } from 'gc-ds-button/loader';
+import 'gc-ds-button/dist/gc-ds-button/gc-ds-button.css'
+
+applyPolyfills().then(() => {
+    defineCustomElements();
+  });
+
 createApp(App).mount('#app')
